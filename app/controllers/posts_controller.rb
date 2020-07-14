bb9c73cb6de180758f0e33f6a9cca5beb88359a6
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-07-14 16:59:59 +0900

    記事編集機能
    what
    記事の編集、削除機能実装
    why
    間違えた投稿を編集、削除するため

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
@@ -14,6 +14,22 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
   end
 
   private
